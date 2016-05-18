69aaa4db35f5bc05382dffd118656e54b95d2606
Steve Honeyman <fvosh@leeds.ac.uk>
2016-05-18 15:16:44 +0100

    Html tidied up

--- page-author.php
+++ page-author.php
@@ -1,12 +1,7 @@
 <?php 
-
-
 /*
-
 Template Name: Author Page
-
 */
-
 
 get_header(); ?>
 
@@ -14,58 +9,44 @@ get_header(); ?>
 
 <!--============== basic loop pulling in page content ==============-->
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<?php the_content(); ?>
+		<?php the_content(); ?>
 
-<?php endwhile; endif; ?>
+	<?php endwhile; endif; ?>
 
 <!--=============== advanced loop =============================-->
 
-<?php
-
-// Get the authors from the database ordered by user nicename
-	global $wpdb;
-	$query = "SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename";
-	$author_ids = $wpdb->get_results($query);
-
-// Loop through each author
-	foreach($author_ids as $author) :
-
-	// Get user data
+	<?php
+		// Get the authors from the database ordered by user nicename
+		global $wpdb;
+		$query = "SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename";
+		$author_ids = $wpdb->get_results($query);
+		// Loop through each author
+		foreach($author_ids as $author) :
+		// Get user data
 		$members = get_userdata($author->ID);
-
-	// If user level is above 0 or login name is "admin", display profile
+		// If user level is above 0 or login name is "admin", display profile
 		if($members->user_level > 0 || $members->user_login == 'admin') :
-
 		// Get link to author page
-			$user_link = get_author_posts_url($members->ID);
+		$user_link = get_author_posts_url($members->ID);
 	?>
 
+			<div class="author-container">
+				<a href="<?php echo $user_link; ?>" title="<?php echo $members->display_name; ?>" class="small-text"><?php echo $members->display_name; ?></a><br />
+				<a href="<?php echo $user_link; ?>" title="<?php echo $members->display_name; ?>">
+					<?php echo get_avatar($members->user_email, '96', $avatar); ?>
+				</a>
+			    
+			<!-- Pull in bio info -->
+					
+				<p> 
+				<?php echo $members->description; ?>
+				</p>
+			</div><!-- close author-container -->
 
-<div class="author-container">
-
-	<a href="<?php echo $user_link; ?>" title="<?php echo $members->display_name; ?>" class="small-text"><?php echo $members->display_name; ?></a><br />
-
-		
-	<a href="<?php echo $user_link; ?>" title="<?php echo $members->display_name; ?>">
-		<?php echo get_avatar($members->user_email, '96', $avatar); ?>
-	</a>
-    
-<!-- code below pulls in bio info -->
-		
-	<p> 
-		<?php echo $members->description; ?>
-	</p>
-
-
-</div>
-
-
-
-<?php endif; ?>
-
-	<?php endforeach; ?>
+			<!--<?php endif; ?>-->
+		<?php endforeach; ?>
 
 
 <?php get_footer(); ?>
